ae722d780b3f562384ed8a1a304f5e837454c3e8
lee <lee@DESKTOP-H5157CI>
2024-01-25 09:47:30 +0900

    3. 정렬 4. 페이징

--- QueryDsl/src/test/java/com/study/querydsl/QuerydslBasicTest.java
+++ QueryDsl/src/test/java/com/study/querydsl/QuerydslBasicTest.java
@@ -1,15 +1,17 @@
 package com.study.querydsl;
 
+import static com.study.querydsl.domain.QMember.member;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.from;
+
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.study.querydsl.domain.Member;
-import static com.study.querydsl.domain.QMember.*;
 import com.study.querydsl.domain.Team;
 
 import jakarta.persistence.EntityManager;
@@ -70,5 +72,49 @@ public class QuerydslBasicTest {
 					.and(member.age.eq(10)))
 					.fetchOne();
 		assertThat(findMember.getUsername()).isEqualTo("member1");
+	}
+	
+	@Test
+	public void sort() {	
+		em.persist(new Member(null, 100));
+		em.persist(new Member("member5", 100));
+		em.persist(new Member("member6", 100));
+		List<Member> result = queryFactory
+				.selectFrom(member)
+				.where(member.age.eq(100))
+				.orderBy(member.age.desc(), member.username.asc().nullsLast())
+				.fetch();
+		
+		Member member5 = result.get(0);
+		Member member6 = result.get(1);
+		Member memberNull = result.get(2);
+		assertThat(member5.getUsername()).isEqualTo("member5");
+		assertThat(member6.getUsername()).isEqualTo("member6");
+		assertThat(memberNull.getUsername()).isNull();
+	}
+	
+	@Test
+	public void paging1() {
+		List<Member> result = queryFactory
+							.selectFrom(member)
+							.orderBy(member.username.desc())
+							.offset(1) // 0부터 시작(zero index
+							.limit(2) // 최대 2건 조회
+							.fetch();
+		assertThat(result.size()).isEqualTo(2);
+	}
+	
+	@Test
+	public void paging2() {
+		QueryResults<Member> queryResults = queryFactory
+					.selectFrom(member)
+					.orderBy(member.username.desc())
+					.offset(1)
+					.limit(2).fetchResults();
+		
+		assertThat(queryResults.getTotal()).isEqualTo(4);
+		assertThat(queryResults.getLimit()).isEqualTo(2);
+		assertThat(queryResults.getOffset()).isEqualTo(1);
+		assertThat(queryResults.getResults().size()).isEqualTo(2);
 	}
 }
