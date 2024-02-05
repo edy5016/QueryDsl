aba03114fb08eae7d2c84826beac333add54eb44
lee <lee@DESKTOP-H5157CI>
2024-02-05 17:02:28 +0900

    7. 스프링 데이터 JPA가 제공하는 Querydsl 기능 	- 3. 리포지토리 지원 - QuerydslRepositorySupport 		+ QuerydslRepositorySupport 		+ getQuerydsl().applyPagination() 스프링 데이터가 제공하는 페이징을 Querydsl로 편리하게 변환가능

--- QueryDsl/src/main/java/com/study/querydsl/repository/MemberRepositoryImpl.java
+++ QueryDsl/src/main/java/com/study/querydsl/repository/MemberRepositoryImpl.java
@@ -8,11 +8,13 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.util.StringUtils;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.study.querydsl.domain.Member;
@@ -22,30 +24,34 @@ import com.study.querydsl.dto.QMemberTeamDto;
 
 import jakarta.persistence.EntityManager;
 
-public class MemberRepositoryImpl implements MemberRepositoryCustom {
+public class MemberRepositoryImpl extends QuerydslRepositorySupport implements MemberRepositoryCustom {
 
 	private final JPAQueryFactory queryFactory;
-	
+//	
+//	public MemberRepositoryImpl(EntityManager em) {
+//		this.queryFactory = new JPAQueryFactory(em);
+//	}
+// ==> QuerydslRepositorySupport 추가시 많은 것들을 제공해줌
 	public MemberRepositoryImpl(EntityManager em) {
-		this.queryFactory = new JPAQueryFactory(em);
+		super(Member.class);
+		this.queryFactory =  new JPAQueryFactory(em);
 	}
-	
+
 	@Override
 	public List<MemberTeamDto> search(MemberSearchCondition condition) {
-		return queryFactory
-					.select(new QMemberTeamDto(
-							member.id,
-							member.username, 
-							member.age, 
-							team.id, team.name))
-					.from(member)
-					.leftJoin(member.team, team)
-					.where(
-							usernameEq(condition.getUsername()), 
-							teamNameEq(condition.getTeamName()),
-							ageGoe(condition.getAgeGoe()), 
-							ageLoe(condition.getAgeLoe()))
-					.fetch();
+		return from(member)
+				.leftJoin(member.team, team)
+				.where(
+						usernameEq(condition.getUsername()), 
+						teamNameEq(condition.getTeamName()),
+						ageGoe(condition.getAgeGoe()), 
+						ageLoe(condition.getAgeLoe()))
+				.select(new QMemberTeamDto(
+						member.id,
+						member.username, 
+						member.age, 
+						team.id, team.name))
+				.fetch();
 	}
 	
 	private BooleanExpression teamNameEq(String teamName) {
@@ -64,27 +70,50 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
 	@Override
 	public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
-		QueryResults<MemberTeamDto> result = queryFactory
-				.select(new QMemberTeamDto(
-						member.id,
-						member.username, 
-						member.age, 
-						team.id, team.name))
-				.from(member)
+//		QueryResults<MemberTeamDto> result = queryFactory
+//				.select(new QMemberTeamDto(
+//						member.id,
+//						member.username, 
+//						member.age, 
+//						team.id, team.name))
+//				.from(member)
+//				.leftJoin(member.team, team)
+//				.where(
+//						usernameEq(condition.getUsername()), 
+//						teamNameEq(condition.getTeamName()),
+//						ageGoe(condition.getAgeGoe()), 
+//						ageLoe(condition.getAgeLoe()))
+//				.offset(pageable.getOffset())
+//				.limit(pageable.getPageSize())
+//				.fetchResults();
+//		
+//		List<MemberTeamDto> contents = result.getResults();
+//		Long total = result.getTotal();
+//		
+//		return new PageImpl<>(contents, pageable, total);
+		
+		JPQLQuery<MemberTeamDto> jpaQuery = 
+				 from(member)
 				.leftJoin(member.team, team)
 				.where(
 						usernameEq(condition.getUsername()), 
 						teamNameEq(condition.getTeamName()),
 						ageGoe(condition.getAgeGoe()), 
 						ageLoe(condition.getAgeLoe()))
-				.offset(pageable.getOffset())
-				.limit(pageable.getPageSize())
-				.fetchResults();
+				.select(new QMemberTeamDto(
+						member.id,
+						member.username, 
+						member.age, 
+						team.id, team.name));
 		
-		List<MemberTeamDto> contents = result.getResults();
-		Long total = result.getTotal();
+		JPQLQuery<MemberTeamDto> query = getQuerydsl().applyPagination(pageable, jpaQuery);
 		
-		return new PageImpl<>(contents, pageable, total);
+		QueryResults<MemberTeamDto> contents = query.fetchResults();
+		Long total = contents.getTotal();
+		
+		
+		return new PageImpl<>(contents.getResults(), pageable, total);
+		
 	}
 
 	@Override
